4fdebffbcadf971829b7c28eadfa01b47222fa5e
manureini <manureini@users.noreply.github.com>
2021-02-27 14:16:10 +0100

    enabled 1x1 cae

--- DFR-source/feat_cae.py
+++ DFR-source/feat_cae.py
@@ -4,71 +4,70 @@ import torch.nn as nn
 #########################################
 #    1 x 1 conv CAE
 #########################################
-#class FeatCAE(nn.Module):
-#    """Autoencoder."""
-#
-#    def __init__(self, in_channels=1000, latent_dim=50, is_bn=True):
-#        super(FeatCAE, self).__init__()
-#
-#        layers = []
-#        layers += [nn.Conv2d(in_channels, (in_channels + 2 * latent_dim) // 2,
-#        kernel_size=1, stride=1, padding=0)]
-#        if is_bn:
-#            layers += [nn.BatchNorm2d(num_features=(in_channels + 2 *
-#            latent_dim) // 2)]
-#        layers += [nn.ReLU()]
-#        layers += [nn.Conv2d((in_channels + 2 * latent_dim) // 2, 2 *
-#        latent_dim, kernel_size=1, stride=1, padding=0)]
-#        if is_bn:
-#            layers += [nn.BatchNorm2d(num_features=2 * latent_dim)]
-#        layers += [nn.ReLU()]
-#        layers += [nn.Conv2d(2 * latent_dim, latent_dim, kernel_size=1,
-#        stride=1, padding=0)]
-#
-#        self.encoder = nn.Sequential(*layers)
-#
-#        # if 1x1 conv to reconstruct the rgb values, we try to learn a linear
-#        combination
-#        # of the features for rgb
-#        layers = []
-#        layers += [nn.Conv2d(latent_dim, 2 * latent_dim, kernel_size=1,
-#        stride=1, padding=0)]
-#        if is_bn:
-#            layers += [nn.BatchNorm2d(num_features=2 * latent_dim)]
-#        layers += [nn.ReLU()]
-#        layers += [nn.Conv2d(2 * latent_dim, (in_channels + 2 * latent_dim) //
-#        2, kernel_size=1, stride=1, padding=0)]
-#        if is_bn:
-#            layers += [nn.BatchNorm2d(num_features=(in_channels + 2 *
-#            latent_dim) // 2)]
-#        layers += [nn.ReLU()]
-#        layers += [nn.Conv2d((in_channels + 2 * latent_dim) // 2, in_channels,
-#        kernel_size=1, stride=1, padding=0)]
-#        # layers += [nn.ReLU()]
-#
-#        self.decoder = nn.Sequential(*layers)
-#
-#    def forward(self, x):
-#        x = self.encoder(x)
-#        x = self.decoder(x)
-#        return x
-#
-#    def relative_euclidean_distance(self, a, b):
-#        return (a - b).norm(2, dim=1) / a.norm(2, dim=1)
-#
-#    def loss_function(self, x, x_hat):
-#        loss = torch.mean((x - x_hat) ** 2)
-#        return loss
-#
-#    def compute_energy(self, x, x_hat):
-#        loss = torch.mean((x - x_hat) ** 2, dim=1)
-#        return loss
-#
+class FeatCAE(nn.Module):
+    """Autoencoder."""
+
+    def __init__(self, in_channels=1000, latent_dim=50, is_bn=True):
+        super(FeatCAE, self).__init__()
+
+        layers = []
+        layers += [nn.Conv2d(in_channels, (in_channels + 2 * latent_dim) // 2,
+        kernel_size=1, stride=1, padding=0)]
+        if is_bn:
+            layers += [nn.BatchNorm2d(num_features=(in_channels + 2 *
+            latent_dim) // 2)]
+        layers += [nn.ReLU()]
+        layers += [nn.Conv2d((in_channels + 2 * latent_dim) // 2, 2 *
+        latent_dim, kernel_size=1, stride=1, padding=0)]
+        if is_bn:
+            layers += [nn.BatchNorm2d(num_features=2 * latent_dim)]
+        layers += [nn.ReLU()]
+        layers += [nn.Conv2d(2 * latent_dim, latent_dim, kernel_size=1,
+        stride=1, padding=0)]
+
+        self.encoder = nn.Sequential(*layers)
+
+        # if 1x1 conv to reconstruct the rgb values, we try to learn a linear combination
+        # of the features for rgb
+        layers = []
+        layers += [nn.Conv2d(latent_dim, 2 * latent_dim, kernel_size=1,
+        stride=1, padding=0)]
+        if is_bn:
+            layers += [nn.BatchNorm2d(num_features=2 * latent_dim)]
+        layers += [nn.ReLU()]
+        layers += [nn.Conv2d(2 * latent_dim, (in_channels + 2 * latent_dim) //
+        2, kernel_size=1, stride=1, padding=0)]
+        if is_bn:
+            layers += [nn.BatchNorm2d(num_features=(in_channels + 2 *
+            latent_dim) // 2)]
+        layers += [nn.ReLU()]
+        layers += [nn.Conv2d((in_channels + 2 * latent_dim) // 2, in_channels,
+        kernel_size=1, stride=1, padding=0)]
+        # layers += [nn.ReLU()]
+
+        self.decoder = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+    def relative_euclidean_distance(self, a, b):
+        return (a - b).norm(2, dim=1) / a.norm(2, dim=1)
+
+    def loss_function(self, x, x_hat):
+        loss = torch.mean((x - x_hat) ** 2)
+        return loss
+
+    def compute_energy(self, x, x_hat):
+        loss = torch.mean((x - x_hat) ** 2, dim=1)
+        return loss
+
 
 #########################################
 #    3 x 3 conv CAE
 #########################################
-class FeatCAE(nn.Module):
+class FeatCAE3x3(nn.Module):
     """Autoencoder."""
 
     def __init__(self, in_channels=1000, latent_dim=50, is_bn=True):
